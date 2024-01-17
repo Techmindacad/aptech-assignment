ee1191a478bb166b75999157ff3b71c194569c22
techmindacad <techmindacad@gmail.com>
2024-01-17 11:34:41 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainGoal from './MainGoal'; 
+import './App.css'; 
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Welcome to My Learning Journey</h1>
+      <MainGoal />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/MainGoal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainGoal.css'; 
+
+const MainGoal = () => {
+  return (
+    <div className="main-goal">
+      <p>My main goal: Learn React in-depth and from the ground up.</p>
+    </div>
+  );
+};
+
+export default MainGoal;
